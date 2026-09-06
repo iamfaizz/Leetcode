6b16a1dcd19873db30759e73c12cb9b0c380b558
Faiz Ahmad <faiz89112@gmail.com>
2026-09-06 14:41:19 +0530

    Time: 11 ms (50.68%) | Memory: 46.2 MB (64.77%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -6,22 +6,17 @@ class Solution {
         int high=0;
         int res=0;
         int max=0;
+
         for(high=0;high<n;high++){
             arr[s.charAt(high)-'A']++;
             max=Math.max(max,arr[s.charAt(high)-'A']);
-            
+
             if(high-low+1-max>k){
                 arr[s.charAt(low)-'A']--;
                 low++;
-
-
             }
-            
             res=Math.max(res,high-low+1);
-
-
         }
         return res;
-        
     }
 }
