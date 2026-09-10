b8562f2a79195e489d1bb5f6c4bf4a8e786c1a68
Faiz Ahmad <faiz89112@gmail.com>
2026-09-10 20:20:59 +0530

    Time: 35 ms (41.27%) | Memory: 59.3 MB (32.02%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -7,6 +7,7 @@ class Solution {
             if(i>0 && nums[i]==nums[i-1]){
                 continue;
             }
+
             int left=i+1;
             int right=n-1;
             int s=-1*nums[i];
@@ -14,24 +15,22 @@ class Solution {
                 int sum=nums[left]+nums[right];
                 if(s==sum){
                     ans.add(Arrays.asList(nums[i],nums[left],nums[right]));
-                    right--;
                     left++;
+                    right--;
                     while(left<n && nums[left]==nums[left-1]){
                         left++;
                     }
                     while(right>=0 && nums[right]==nums[right+1]){
                         right--;
                     }
-
                 }else if(sum<s){
                     left++;
-
                 }else{
                     right--;
                 }
             }
-
         }
         return ans;
+
     }
 }
