f64dfc0cd73cbf7fb11ee04fb0110bbce6eba5cd
Faiz Ahmad <faiz89112@gmail.com>
2026-09-17 23:28:29 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (47.10%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -11,30 +11,26 @@ class Solution {
             if(nums[mid]==target){
                 return mid;
             }
+
             if(nums[mid]>nums[n-1]){
                 if(nums[mid]<target){
                     low=mid+1;
                 }else if(nums[0]>target){
                     low=mid+1;
-                }
-                else{
+                }else{
                     high=mid-1;
+
                 }
-                continue;
-                
             }else{
                 if(nums[mid]>target){
                     high=mid-1;
+                }else if(nums[n-1]<target){
+                    high=mid-1;;
                 }else{
-                    if(nums[n-1]<target){
-                        high=mid-1;
-                    }else{
-                        low=mid+1;
-
-                    }
+                    low=mid+1;
                 }
+
             }
-           
         }
         return -1;
     }
